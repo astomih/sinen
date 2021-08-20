dc8fbb6312d475f8937b41c24076981acfcbccf0
Astomih <asamin1090@gmail.com>
2021-08-21 01:37:32 +0900

    反映用

---
+++ include/nen/Actor/Text.hpp
@@ -0,0 +1,20 @@
+#pragma once
+#include "Actor.hpp"
+#include "../Color/Color.hpp"
+#include <string>
+#include <memory>
+namespace nen
+{
+	class Text : public Actor
+	{
+	public:
+		Text(class Scene &scene);
+		void SetText(const std::string &text, const int size = 10) const;
+		int GetWidth() const;
+		int GetHeight() const;
+		void SetColor(const Color &color);
+
+	private:
+		std::shared_ptr<class FontComponent> mFont;
+	};
+}

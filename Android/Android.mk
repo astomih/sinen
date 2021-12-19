8924ed9cb971ad3ad080deb35aab38144cc751e0
Astomih <asamin1090@gmail.com>
2021-12-19 22:50:55 +0900

    Andoid.mkを移動

--- Android/Android.mk
+++ Android/Android.mk
@@ -2,32 +2,32 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := nen
+LOCAL_MODULE := Nen
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH) \
-                    $(LOCAL_PATH)/../include \
-                    $(LOCAL_PATH)/../include/nen \
-                    $(LOCAL_PATH)/../libs/ImGui/include \
-                    $(LOCAL_PATH)/../libs/Lua/include \
-                    $(LOCAL_PATH)/../libs/mojoAL \
-                    $(LOCAL_PATH)/../libs/rapidjson/include \
-                    $(LOCAL_PATH)/../libs/sol2/include \
+                    $(LOCAL_PATH)/NenEngine/include \
+                    $(LOCAL_PATH)/NenEngine/include/Nen \
+                    $(LOCAL_PATH)/NenEngine/libs/ImGui/include \
+                    $(LOCAL_PATH)/NenEngine/libs/Lua/include \
+                    $(LOCAL_PATH)/NenEngine/libs/mojoAL \
+                    $(LOCAL_PATH)/NenEngine/libs/rapidjson/include \
+                    $(LOCAL_PATH)/NenEngine/libs/sol2/include \
 
-LOCAL_CFLAGS := -DSOL_NO_EXCEPTIONS
-SRC_PATH := $(LOCAL_PATH)/../src/nen \
-            $(LOCAL_PATH)/../libs/ImGui/source \
-            $(LOCAL_PATH)/../libs/Lua/source \
-            $(LOCAL_PATH)/../libs/mojoAL \
+LOCAL_CFLAGS := -DSOL_NO_EXCEPTIONS -DNEN_NO_EXCEPTION -DMOBILE
+SRC_PATH := $(LOCAL_PATH)/src/Nen \
+            $(LOCAL_PATH)/NenEngine/libs/ImGui/source \
+            $(LOCAL_PATH)/NenEngine/libs/Lua/source \
+            $(LOCAL_PATH)/NenEngine/libs/mojoAL
 
 LOCAL_SRC_FILES := \
-    $(wildcard $(LOCAL_PATH)/../src/nen/*/*.c) \
-    $(wildcard $(LOCAL_PATH)/../src/nen/Render/OpenGLES/*.c) \
-    $(wildcard $(LOCAL_PATH)/../libs/ImGui/source/*.c) \
-    $(wildcard $(LOCAL_PATH)/../libs/Lua/source/*.c) \
-    $(wildcard $(LOCAL_PATH)/../libs/mojoAL/mojoal.c) \
-
+    $(wildcard $(LOCAL_PATH)/NenEngine/src/nen/*/*.cpp) \
+    $(wildcard $(LOCAL_PATH)/NenEngine/src/nen/Render/OpenGLES/*.cpp) \
+    $(wildcard $(LOCAL_PATH)/NenEngine/libs/ImGui/source/gles/*.cpp) \
+    $(wildcard $(LOCAL_PATH)/NenEngine/libs/Lua/source/*.c) \
+    $(wildcard $(LOCAL_PATH)/NenEngine/libs/mojoAL/mojoal.c)
+LOCAL_STATIC_LIBRARIES :=
 LOCAL_SHARED_LIBRARIES := SDL2 SDL2_image SDL2_mixer SDL2_ttf SDL2_net
 LOCAL_LDLIBS := -lGLESv3 -llog
-LOCAL_EXPORT_C_INCLUDES += $(LOCAL_C_INCLUDES) \
+LOCAL_EXPORT_C_INCLUDES += $(LOCAL_C_INCLUDES)
 
 include $(BUILD_SHARED_LIBRARY)

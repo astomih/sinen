40ac3ba8a91d2ef3d45e622568b71682faa3610f
astomih <asamin1090@gmail.com>
2022-01-28 12:42:19 +0000

    Deploying to gh-pages from @ astomih/sinen@f4ccf91e0ea2168ca945bab8dc344a556406929a 🚀

--- search/all_1.js
+++ search/all_1.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['base_5factor_1',['base_actor',['../classnen_1_1base__actor.html',1,'nen::base_actor'],['../classnen_1_1base__actor.html#a386e0b2286b18ff44b2b91db36ae1724',1,'nen::base_actor::base_actor()']]],
+  ['base_5factor_1',['base_actor',['../classnen_1_1base__actor.html#a386e0b2286b18ff44b2b91db36ae1724',1,'nen::base_actor::base_actor()'],['../classnen_1_1base__actor.html',1,'nen::base_actor']]],
   ['base_5fcomponent_2',['base_component',['../classnen_1_1base__component.html',1,'nen']]],
   ['base_5fscene_3',['base_scene',['../classnen_1_1base__scene.html',1,'nen']]],
   ['billboard_5fcomponent_4',['billboard_component',['../classnen_1_1billboard__component.html',1,'nen']]],

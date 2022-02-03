a3b4418ebce8ad58e2c38771c15305d056f8fb61
Astomih <asamin1090@gmail.com>
2022-02-03 10:37:55 +0900

    update

--- src/Nen/Physics/Collision.cpp
+++ src/Nen/Physics/Collision.cpp
@@ -3,13 +3,19 @@
 namespace nen {
 int collision::IntersectAABB(const vector3 &a, const vector3 &b,
                              const vector3 &detectSpace) {
-  int i = 0b111;
-  if (!(a.x - detectSpace.x <= b.x && b.x <= a.x + detectSpace.x))
-    i -= 0b001;
-  if (!(a.y - detectSpace.y <= b.y && b.y <= a.y + detectSpace.y))
-    i -= 0b010;
-  if (!(a.z - detectSpace.z <= b.z && b.z <= a.z + detectSpace.z))
-    i -= 0b100;
+  int i = 7;
+  if (!(a.x - detectSpace.x <= b.x && b.x <= a.x + detectSpace.x)) {
+    i -= 1;
+    return i;
+  }
+  if (!(a.y - detectSpace.y <= b.y && b.y <= a.y + detectSpace.y)) {
+    i -= 2;
+    return i;
+  }
+  if (!(a.z - detectSpace.z <= b.z && b.z <= a.z + detectSpace.z)) {
+    i -= 4;
+    return i;
+  }
   return i;
 }
 } // namespace nen

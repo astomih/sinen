5a216605279141845e3dfb5faa04d8d09cf16d24
astomih <asamin1090@gmail.com>
2022-03-02 04:56:19 +0000

    Deploying to gh-pages from @ astomih/sinen@cdfe6f507246b114ad63ffb3d7349e4d34500eeb 🚀

--- search/functions_1.js
+++ search/functions_1.js
@@ -1,7 +1,7 @@
 var searchData=
 [
-  ['initialize_158',['Initialize',['../classnen_1_1scene.html#aa6e5f5a63d5c90be208b04f8e6a88094',1,'nen::scene']]],
-  ['initialize_159',['initialize',['../classnen_1_1manager.html#a7959b7af4db66841672333e4e1fad1d9',1,'nen::manager']]],
+  ['initialize_158',['initialize',['../classnen_1_1manager.html#a7959b7af4db66841672333e4e1fad1d9',1,'nen::manager']]],
+  ['initialize_159',['Initialize',['../classnen_1_1scene.html#aa6e5f5a63d5c90be208b04f8e6a88094',1,'nen::scene']]],
   ['isloaded_160',['isLoaded',['../classnen_1_1font.html#af5a004c042f7aa51c606c894ba4be8fa',1,'nen::font']]],
   ['isrunning_161',['isRunning',['../classnen_1_1scene.html#a19b8eac4f43c72c6a2ad46e26a64036d',1,'nen::scene']]]
 ];

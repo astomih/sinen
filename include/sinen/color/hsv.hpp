a285460be0d2284cec13a74e05df4ca5c8e600eb
Astomih <asamin1090@gmail.com>
2022-09-05 19:47:01 +0900

    Solitting a hsv implementation

--- include/sinen/color/hsv.hpp
+++ include/sinen/color/hsv.hpp
@@ -22,65 +22,8 @@ struct hsv {
   hsv(float h, float s, float v) : h(h), s(s), v(v) {}
   hsv() : hsv(0, 0, 0) {}
   hsv(const color &c) { from_color(c); }
-  void from_color(const color &c) {
-    float r = c.r;
-    float g = c.g;
-    float b = c.b;
-    float max = std::max(r, std::max(g, b));
-    float min = std::min(r, std::min(g, b));
-    float delta = max - min;
-    if (delta == 0) {
-      h = 0;
-    } else if (max == r) {
-      h = 60 * (g - b) / delta;
-    } else if (max == g) {
-      h = 60 * (b - r) / delta + 120;
-    } else if (max == b) {
-      h = 60 * (r - g) / delta + 240;
-    }
-    if (h < 0) {
-      h += 360;
-    }
-    if (max == 0) {
-      s = 0;
-    } else {
-      s = delta / max;
-    }
-    v = max;
-  }
-  color to_color() const {
-    float r, g, b;
-    float c = v * s;
-    float h_ = h / 60;
-    float x = c * (1 - std::abs(std::fmod(h_, 2) - 1));
-    if (h_ < 1) {
-      r = c;
-      g = x;
-      b = 0;
-    } else if (h_ < 2) {
-      r = x;
-      g = c;
-      b = 0;
-    } else if (h_ < 3) {
-      r = 0;
-      g = c;
-      b = x;
-    } else if (h_ < 4) {
-      r = 0;
-      g = x;
-      b = c;
-    } else if (h_ < 5) {
-      r = x;
-      g = 0;
-      b = c;
-    } else {
-      r = c;
-      g = 0;
-      b = x;
-    }
-    float m = v - c;
-    return color((r + m), (g + m), (b + m), 1);
-  }
+  void from_color(const color &c);
+  color to_color() const;
   operator color() const { return to_color(); }
 };
 } // namespace sinen
---
+++ src/color/hsv.cpp
@@ -0,0 +1,64 @@
+#include <color/hsv.hpp>
+namespace sinen {
+void hsv::from_color(const color &c) {
+
+  float r = c.r;
+  float g = c.g;
+  float b = c.b;
+  float max = std::max(r, std::max(g, b));
+  float min = std::min(r, std::min(g, b));
+  float delta = max - min;
+  if (delta == 0) {
+    h = 0;
+  } else if (max == r) {
+    h = 60 * (g - b) / delta;
+  } else if (max == g) {
+    h = 60 * (b - r) / delta + 120;
+  } else if (max == b) {
+    h = 60 * (r - g) / delta + 240;
+  }
+  if (h < 0) {
+    h += 360;
+  }
+  if (max == 0) {
+    s = 0;
+  } else {
+    s = delta / max;
+  }
+  v = max;
+}
+
+color hsv::to_color() const {
+  float r, g, b;
+  float c = v * s;
+  float h_ = h / 60;
+  float x = c * (1 - std::abs(std::fmod(h_, 2) - 1));
+  if (h_ < 1) {
+    r = c;
+    g = x;
+    b = 0;
+  } else if (h_ < 2) {
+    r = x;
+    g = c;
+    b = 0;
+  } else if (h_ < 3) {
+    r = 0;
+    g = c;
+    b = x;
+  } else if (h_ < 4) {
+    r = 0;
+    g = x;
+    b = c;
+  } else if (h_ < 5) {
+    r = x;
+    g = 0;
+    b = c;
+  } else {
+    r = c;
+    g = 0;
+    b = x;
+  }
+  float m = v - c;
+  return color((r + m), (g + m), (b + m), 1);
+}
+} // namespace sinen

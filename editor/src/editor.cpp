4f7d8b824a53845f6c4a03b13d3afb9e63d4215b
astomih <asamin1090@gmail.com>
2022-09-26 22:12:56 +0900

    Added editor, Update GitHub Actions workflow

---
+++ editor/src/editor.cpp
@@ -0,0 +1,8 @@
+#include "editor.hpp"
+#include <sinen/render/renderer.hpp>
+namespace sinen {
+editor::editor() {}
+editor::~editor() {}
+void editor::setup() {}
+void editor::update(float delta_time) {}
+} // namespace sinen
---
+++ editor/src/editor.hpp
@@ -0,0 +1,35 @@
+#ifndef SINEN_EDITOR_HPP
+#define SINEN_EDITOR_HPP
+#include <sinen/scene/scene.hpp>
+
+namespace sinen {
+/**
+ * @brief Editor
+ *
+ */
+class editor : public scene {
+public:
+  /**
+   * @brief Construct a new editor object
+   *
+   */
+  editor();
+  /**
+   * @brief Destroy the editor object
+   *
+   */
+  ~editor();
+  /**
+   * @brief Setup
+   *
+   */
+  void setup() override;
+  /**
+   * @brief Update the editor
+   *
+   * @param delta_time Delta time
+   */
+  void update(float delta_time) override;
+};
+} // namespace sinen
+#endif

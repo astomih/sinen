a185fb6465e7df31b3d83657c506bbd23da5a89c
astomih <asamin1090@gmail.com>
2022-10-08 16:14:10 +0900

    Update en2ja

--- tools/en2ja/src/main.cpp
+++ tools/en2ja/src/main.cpp
@@ -2,33 +2,24 @@
 // for SetConsoleOutput
 #include <Windows.h>
 #endif
+#include <cstdio>
+#include <cstdlib>
 #include <filesystem>
 #include <fstream>
 #include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
 #include <string>
-#include <tchar.h>
 #include <unordered_map>
 #include <vector>
 
-const std::string En2JaVersion = "0.0.1";
-void inputWord(std::string &word) {
+const std::string en2ja_version = "0.0.1";
+void input_word(std::string &word) {
   std::cout << reinterpret_cast<const char *>(
                    u8"英単語を入力してください(小文字)  \"Exit\":終了, "
+                   u8"\"Log\":履歴を表示, "
                    u8"\"Print\":output.txtに記録, \"Help\"：ヘルプを表示")
             << std::endl;
   std::cin >> word;
 }
-bool findMap(std::unordered_map<std::string, std::string> &map,
-             const std::string &word) {
-  if (map.find(word) != map.end()) {
-    std::cout << map[word];
-    return true;
-  }
-  return false;
-}
-
 void init(std::string &, std::string &,
           std::unordered_map<std::string, std::string> &);
 
@@ -37,6 +28,9 @@ void not_found() {
                    u8"\n該当する英単語が見つかりませんでした。")
             << std::endl;
 }
+bool loop(std::string &word, std::vector<std::string> &key_buffer,
+          std::ofstream &ofs,
+          std::unordered_map<std::string, std::string> &map);
 
 int main(int argc, char **argv) {
 #ifdef _WIN32
@@ -45,65 +39,25 @@ int main(int argc, char **argv) {
   std::string key{};
   std::string value{};
   std::unordered_map<std::string, std::string> map;
-  std::vector<std::string> keyBuffer;
+  std::vector<std::string> key_buffer;
   std::ofstream ofs("./output.txt", std::ios_base::app | std::ios_base::out);
   if (ofs.bad()) {
     std::cout << "Error" << std::endl;
   }
   init(key, value, map);
-
   std::string word;
-  if (argc < 2)
-    inputWord(word);
-  else
-    word = std::string(argv[1]);
-  if (word == "Exit")
+  if (argc > 1) {
+    for (int i = 2; i < argc; i++) {
+      word = std::string(argv[i]);
+      if (!loop(word, key_buffer, ofs, map))
+        break;
+    }
     return 0;
-  else if (word == "Print")
-    std::cout << reinterpret_cast<const char *>(u8"まだ何も入力されていません")
-              << std::endl;
-  else if (word == "Help") {
-    std::cout << "En2Ja Version:" << En2JaVersion << std::endl;
-    std::cout << "" << std::endl;
-  } else if (findMap(map, word)) {
-    keyBuffer.push_back(word);
-  } else
-    not_found();
-  std::cout << std::endl;
-  for (int i = 2; i < argc; i++) {
-    word = std::string(argv[i]);
-    if (word == "Exit")
-      return 0;
-    if (word == "Print") {
-      for (auto &i : keyBuffer) {
-        ofs << i << ":"
-            << "\n";
-        ofs << map[i];
-        ofs << "\n";
-      }
-    } else if (findMap(map, word)) {
-      keyBuffer.push_back(word);
-    } else
-      not_found();
-    std::cout << std::endl;
   }
-  while (true) {
-    inputWord(word);
-    if (word == "Exit")
-      break;
-    if (word == "Print") {
-      for (auto &i : keyBuffer) {
-        ofs << i << ":"
-            << "\n";
-        ofs << map[i];
-        ofs << "\n";
-      }
-    } else if (findMap(map, word)) {
-      keyBuffer.push_back(word);
-    } else
-      not_found();
-    std::cout << std::endl;
-  }
+  do {
+    input_word(word);
+    std::system("cls");
+  } while (loop(word, key_buffer, ofs, map));
   ofs.close();
   return 0;
 }
@@ -139,4 +93,50 @@ void init(std::string &key, std::string &value,
     if (c == EOF)
       break;
   }
+}
+
+bool loop(std::string &word, std::vector<std::string> &key_buffer,
+          std::ofstream &ofs,
+          std::unordered_map<std::string, std::string> &map) {
+  if (word == "Exit")
+    return false;
+  if (word == "Log") {
+    if (key_buffer.empty()) {
+      std::cout << reinterpret_cast<const char *>(u8"履歴がありません。")
+                << std::endl;
+      return true;
+    }
+    for (auto &i : key_buffer) {
+      std::cout << i << ":"
+                << "\n"
+                << map[i] << "\n";
+    }
+    return true;
+  }
+  if (word == "Print") {
+    if (key_buffer.empty()) {
+      std::cout << reinterpret_cast<const char *>(u8"履歴がありません。")
+                << std::endl;
+      return true;
+    }
+    for (auto &i : key_buffer) {
+      ofs << i << ":"
+          << "\n";
+      ofs << map[i];
+      ofs << "\n";
+    }
+    return true;
+  }
+  if (word == "Help") {
+    std::cout << "en2ja Version:" << en2ja_version << std::endl;
+    return true;
+  }
+  if (map.contains(word)) {
+
+    std::cout << word << " :\n" << map[word] << std::endl;
+    key_buffer.push_back(word);
+    return true;
+  }
+  not_found();
+  return true;
 }

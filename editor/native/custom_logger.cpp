9086ff32cb82cf30074b7e313416fbdb6b1f7416
astomih <astomih.jp@gmail.com>
2025-01-08 20:35:54 +0900

    Update: Enhance custom logger with detailed output for each priority level

--- editor/native/custom_logger.cpp
+++ editor/native/custom_logger.cpp
@@ -11,21 +11,27 @@ void custom_logger() {
         switch (priority) {
         case Logger::priority::verbose:
           color = ImVec4(0.0f, 1.0f, 1.0f, 1.0f);
+          std::cout << "VERBOSE: " << str << std::endl;
           break;
         case Logger::priority::debug:
           color = ImVec4(0.0f, 0.0f, 1.0f, 1.0f);
+          std::cout << "DEBUG: " << str << std::endl;
           break;
         case Logger::priority::info:
           color = ImVec4(0.0f, 1.0f, 0.0f, 1.0f);
+          std::cout << "INFO: " << str << std::endl;
           break;
         case Logger::priority::error:
           color = ImVec4(1.0f, 0.0f, 0.0f, 1.0f);
+          std::cout << "ERROR: " << str << std::endl;
           break;
         case Logger::priority::warn:
           color = ImVec4(1.0f, 1.0f, 0.0f, 1.0f);
+          std::cout << "WARNING: " << str << std::endl;
           break;
         case Logger::priority::critical:
           color = ImVec4(1.0f, 0.0f, 1.0f, 1.0f);
+          std::cout << "CRITICAL: " << str << std::endl;
           break;
         default:
           color = ImVec4(1.0f, 1.0f, 1.0f, 1.0f);

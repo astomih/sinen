da520cdbc67b53ff196e5c65df8395b6a2c2031e
astomih <astomih.jp@gmail.com>
2025-02-27 22:10:36 +0900

    Remove: vertex.cpp

--- src/sinen/model/vertex.cpp
+++
@@ -1,6 +0,0 @@
-#include "vertex/vertex.hpp"
-#include <string_view>
-namespace sinen {
-std::string_view VertexDefaultShapes::sprite = "SPRITE";
-std::string_view VertexDefaultShapes::box = "BOX";
-} // namespace sinen

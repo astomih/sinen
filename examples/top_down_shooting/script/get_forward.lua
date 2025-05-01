58121aa02e65e906255e733397ba31dddecc9195
astomih <astomih.jp@gmail.com>
2025-05-02 02:00:21 +0900

    delete stg example, moved to works repo

--- examples/top_down_shooting/script/get_forward.lua
+++
@@ -1,7 +0,0 @@
--- Get the objects forward vector
-local function get_forward(drawer)
-    return Vec3(-math.sin(math.rad(drawer.rotation.z)),
-        math.cos(math.rad(-drawer.rotation.z)), 1)
-end
-
-return get_forward
--- examples/top_down_shooting/script/gui/global.lua
+++
@@ -1,4 +0,0 @@
-UI_SCALE = 1.0
-function UI_SCALE_Vec2(x, y)
-  return Vec2(x * UI_SCALE, y * UI_SCALE)
-end

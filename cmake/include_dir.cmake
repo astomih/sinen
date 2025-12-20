795ab783e38638b56c667d27e0335378c4d9c09f
astomih <astomih.jp@gmail.com>
2025-12-20 23:34:02 +0900

    format

--- cmake/include_dir.cmake
+++ cmake/include_dir.cmake
@@ -1,37 +1,31 @@
 set(SINEN_INCLUDE_DIRS
-        ${Vulkan_INCLUDE_DIR}
-        ${SINEN_SOURCE_DIR}
-        ${SINEN_SOURCE_DIR}/src
-        ${SINEN_SOURCE_DIR}/libs/imgui
-        ${SINEN_SOURCE_DIR}/libs/imgui/backends
-        ${SINEN_SOURCE_DIR}/libs/rapidjson/include
-        ${SINEN_SOURCE_DIR}/libs/SDL/include
-        ${SINEN_SOURCE_DIR}/libs/miniaudio
-        ${SINEN_SOURCE_DIR}/src/script/include
-        ${SINEN_SOURCE_DIR}/src/rt_shader_compiler/include
-        ${SINEN_SOURCE_DIR}/libs/assimp/include
-        ${SINEN_SOURCE_DIR}/libs/glm
-        ${SINEN_SOURCE_DIR}/libs/SDL/include
-        ${SINEN_SOURCE_DIR}/libs/sol2/include
-        ${SINEN_SOURCE_DIR}/libs/stb
-        ${SINEN_SOURCE_DIR}/libs/slang/include
-        ${SINEN_SOURCE_DIR}/libs/JoltPhysics
-        ${SINEN_SOURCE_DIR}/libs/LuaJIT/src
-        ${SINEN_SOURCE_DIR}/libs/tinyexr
-        ${SINEN_SOURCE_DIR}/libs/tinyexr/deps/miniz
-        ${SINEN_SOURCE_DIR}/libs/tlsf
-        ${SINEN_SOURCE_DIR}/libs/volk
-        ${SINEN_SOURCE_DIR}/libs/VulkanMemoryAllocator/include
-)
+    ${Vulkan_INCLUDE_DIR}
+    ${SINEN_SOURCE_DIR}
+    ${SINEN_SOURCE_DIR}/src
+    ${SINEN_SOURCE_DIR}/libs/imgui
+    ${SINEN_SOURCE_DIR}/libs/imgui/backends
+    ${SINEN_SOURCE_DIR}/libs/rapidjson/include
+    ${SINEN_SOURCE_DIR}/libs/SDL/include
+    ${SINEN_SOURCE_DIR}/libs/miniaudio
+    ${SINEN_SOURCE_DIR}/src/script/include
+    ${SINEN_SOURCE_DIR}/src/rt_shader_compiler/include
+    ${SINEN_SOURCE_DIR}/libs/assimp/include
+    ${SINEN_SOURCE_DIR}/libs/glm
+    ${SINEN_SOURCE_DIR}/libs/SDL/include
+    ${SINEN_SOURCE_DIR}/libs/sol2/include
+    ${SINEN_SOURCE_DIR}/libs/stb
+    ${SINEN_SOURCE_DIR}/libs/slang/include
+    ${SINEN_SOURCE_DIR}/libs/JoltPhysics
+    ${SINEN_SOURCE_DIR}/libs/LuaJIT/src
+    ${SINEN_SOURCE_DIR}/libs/tinyexr
+    ${SINEN_SOURCE_DIR}/libs/tinyexr/deps/miniz
+    ${SINEN_SOURCE_DIR}/libs/tlsf
+    ${SINEN_SOURCE_DIR}/libs/volk
+    ${SINEN_SOURCE_DIR}/libs/VulkanMemoryAllocator/include)
 
-if (EMSCRIPTEN)
-    list(APPEND SINEN_INCLUDE_DIRS
-            ${SINEN_SOURCE_DIR}/libs/lua
-    )
-else ()
-    list(APPEND SINEN_INCLUDE_DIRS
-            ${SINEN_SOURCE_DIR}/libs/LuaJIT/src
-            ${SINEN_BINARY_DIR}/LuaJIT
-            ${SINEN_BINARY_DIR}/LuaJIT/src
-    )
-endif ()
+if(EMSCRIPTEN)
+  list(APPEND SINEN_INCLUDE_DIRS ${SINEN_SOURCE_DIR}/libs/lua)
+else()
+  list(APPEND SINEN_INCLUDE_DIRS ${SINEN_SOURCE_DIR}/libs/LuaJIT/src
+       ${SINEN_BINARY_DIR}/LuaJIT ${SINEN_BINARY_DIR}/LuaJIT/src)
+endif()

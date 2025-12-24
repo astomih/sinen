253fe751f230aa1f7d840bc8b2ec2d9e7d6bb68f
astomih <astomih.jp@gmail.com>
2025-12-24 15:00:39 +0900

    Update macro

--- src/core/def/macro.hpp
+++ src/core/def/macro.hpp
@@ -11,12 +11,19 @@
 // Macro to define the platform
 #ifdef _WIN32
 #define SINEN_PLATFORM_WINDOWS
+#define SINEN_PLATFORM_DESKTOP
 #elif __linux__
 #define SINEN_PLATFORM_LINUX
+#define SINEN_PLATFORM_DESKTOP
 #elif __APPLE__
 #define SINEN_PLATFORM_MACOS
+#define SINEN_PLATFORM_DESKTOP
+#elif __ANDROID__
+#define SINEN_PLATFORM_ANDROID
+#define SINEN_PLATFORM_MOBILE
 #elif __EMSCRIPTEN__
 #define SINEN_PLATFORM_EMSCRIPTEN
+#define SINEN_PLATFORM_WEB
 #endif
 
 #endif // !SINEN_MACRO_HPP

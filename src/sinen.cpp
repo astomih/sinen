3bc97464fd794b4e16e15725a151d4c644afacbf
astomih <astomih.jp@gmail.com>
2026-01-08 14:50:03 +0900

    Replace main callback system

--- src/sinen.cpp
+++ src/sinen.cpp
@@ -10,6 +10,7 @@
 
 #include <sinen.hpp>
 
+#define SDL_MAIN_USE_CALLBACKS
 #include <SDL3/SDL_main.h>
 
 #include <SDL3/SDL.h>
@@ -29,7 +30,7 @@ struct ImGuiLog {
 Array<ImGuiLog::Type> ImGuiLog::logs =
     Array<ImGuiLog::Type>(GlobalAllocator::get());
 
-int main(const int argc, char *argv[]) {
+SDL_AppResult SDL_AppInit(void **appstate, int argc, char **argv) {
   Arguments::argc = argc;
   Arguments ::argv.resize(argc);
   for (int i = 0; i < argc; i++) {
@@ -41,7 +42,7 @@ int main(const int argc, char *argv[]) {
   SDL_InitFlags initFlags = SDL_INIT_AUDIO | SDL_INIT_EVENTS;
   if (!SDL_Init(initFlags)) {
     Logger::critical("Failed to initialize SDL");
-    return false;
+    return SDL_APP_FAILURE;
   }
 
   Logger::setOutputFunction([&](Logger::priority p, StringView str) {
@@ -81,77 +82,80 @@ int main(const int argc, char *argv[]) {
 
   if (!WindowSystem::initialize("Sinen")) {
     Logger::critical("Failed to initialize window system");
-    return false;
+    return SDL_APP_FAILURE;
   }
   GraphicsSystem::initialize();
   if (!AudioSystem::initialize()) {
     Logger::critical("Failed to initialize audio system");
     AudioSystem::shutdown();
-    return false;
+    return SDL_APP_FAILURE;
   }
   if (!InputSystem::initialize()) {
     Logger::critical("Failed to initialize input system");
-    return false;
+    return SDL_APP_FAILURE;
   }
   if (!PhysicsSystem::initialize()) {
     Logger::critical("Failed to initialize physics system");
-    return false;
+    return SDL_APP_FAILURE;
   }
   if (!ScriptSystem::initialize()) {
     Logger::critical("Failed to initialize script system");
-    return false;
+    return SDL_APP_FAILURE;
   }
   if (!RandomSystem::initialize()) {
     Logger::critical("Failed to initialize random system");
-    return false;
+    return SDL_APP_FAILURE;
   }
-  auto setup = []() {
+  return SDL_APP_CONTINUE;
+}
+SDL_AppResult SDL_AppIterate(void *appstate) {
+  if (EventSystem::isQuit())
+    return SDL_APP_SUCCESS;
+  InputSystem::update();
+  {
+    if (Keyboard::isPressed(Keyboard::Code::F3) ||
+        KeyInput::isPressed(KeyInput::AC_BACK)) {
+      Graphics::toggleShowImGui();
+      if (Graphics::isShowImGui()) {
+        Graphics::addImGuiFunction([&]() {
+          // Log Window
+          ImGui::SetNextWindowPos(ImVec2(0, Window::size().y * (3.f / 4.f)),
+                                  ImGuiCond_Always);
+          ImGui::SetNextWindowSize(
+              ImVec2(Window::size().x, Window::size().y * (1.f / 4.f)),
+              ImGuiCond_Always);
+          ImGui::Begin("Log", nullptr, ImGuiWindowFlags_NoResize);
+          for (auto &log : ImGuiLog::logs) {
+            ImGui::TextColored(log.color, "%s", (log.str).c_str());
+          }
+          ImGui::End();
+        });
+      } else {
+        Graphics::getImGuiFunction().clear();
+      }
+    }
+  }
+  Time::update();
+  ScriptSystem::updateScene();
+  PhysicsSystem::update();
+  GraphicsSystem::render();
+  if (ScriptSystem::hasToReload()) {
     ScriptSystem::runScene();
     PhysicsSystem::postSetup();
     ScriptSystem::doneReload();
-  };
-  while (!EventSystem::isQuit()) {
-    WindowSystem::prepareFrame();
-    InputSystem::prepareForUpdate();
-    SDL_Event event;
-    while (SDL_PollEvent(&event)) {
-      ImGui_ImplSDL3_ProcessEvent(&event);
-      EventSystem::processEvent(event);
-      WindowSystem::processInput(event);
-      InputSystem::processEvent(event);
-    }
-    InputSystem::update();
-    {
-      if (Keyboard::isPressed(Keyboard::Code::F3) ||
-          KeyInput::isPressed(KeyInput::AC_BACK)) {
-        Graphics::toggleShowImGui();
-        if (Graphics::isShowImGui()) {
-          Graphics::addImGuiFunction([&]() {
-            // Log Window
-            ImGui::SetNextWindowPos(ImVec2(0, Window::size().y * (3.f / 4.f)),
-                                    ImGuiCond_Always);
-            ImGui::SetNextWindowSize(
-                ImVec2(Window::size().x, Window::size().y * (1.f / 4.f)),
-                ImGuiCond_Always);
-            ImGui::Begin("Log", nullptr, ImGuiWindowFlags_NoResize);
-            for (auto &log : ImGuiLog::logs) {
-              ImGui::TextColored(log.color, "%s", (log.str).c_str());
-            }
-            ImGui::End();
-          });
-        } else {
-          Graphics::getImGuiFunction().clear();
-        }
-      }
-    }
-    Time::update();
-    ScriptSystem::updateScene();
-    PhysicsSystem::update();
-    GraphicsSystem::render();
-    if (ScriptSystem::hasToReload()) {
-      setup();
-    }
   }
+  WindowSystem::prepareFrame();
+  InputSystem::prepareForUpdate();
+  return SDL_APP_CONTINUE;
+}
+SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
+  ImGui_ImplSDL3_ProcessEvent(event);
+  EventSystem::processEvent(*event);
+  WindowSystem::processInput(*event);
+  InputSystem::processEvent(*event);
+  return SDL_APP_CONTINUE;
+}
+void SDL_AppQuit(void *appstate, SDL_AppResult result) {
   PhysicsSystem::shutdown();
   InputSystem::shutdown();
   AudioSystem::shutdown();
@@ -160,5 +164,4 @@ int main(const int argc, char *argv[]) {
   WindowSystem::shutdown();
   ScriptSystem::shutdown();
   SDL_Quit();
-  return 0;
 }
